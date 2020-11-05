204ccbd385bfc7b2ff906ef055c2e32f2b88c52e
yyyey <dmsdud322@naver.com>
2020-11-05 17:40:10 +0900

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yyy:yyy1234@bolierplate.kzb9l.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!~~')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+
