b75c3f2aa7b11d76f05ca6da6327d78833c55fe6
yyyey <dmsdud322@naver.com>
2020-11-11 16:32:27 +0900

    auth 라우트 완성

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원 가입 할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
@@ -82,10 +82,33 @@ app.post('/api/users/login', (req, res) => {
   })
 })
 
-  //비밀번호 까지 맞다면 토큰을 생성하기.
+// role 1 admin   role 2 특정부서admin
+// role 0 일반유저   role 0 아니면 관리자
+app.get('/api/users/auth', auth, (req,res) => {
 
+  // 여기까지 미드웨어를 통과해 왔다는 얘기는 authentication 이 true 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
 
-
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id},
+    {token: ""},
+    (err, user) =>{
+      if(err) return res.json({ success:false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
